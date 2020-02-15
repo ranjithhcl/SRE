adae27c168cb905f6e0f0b3e8c3c32236b3e8258
Ranjith KA <ranjith.ka@dynacommerce.com>
2020-02-16 00:53:58 +0530

    Add git in the container to download open sorce repos

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,3 +7,27 @@ module "key_pair" {
   create_key_pair = true
   key_name        = "mykey"
 }
+
+module "vpc" {
+  source = "github.com/terraform-aws-modules/terraform-aws-vpc"
+
+  name = var.project
+  cidr = "10.1.0.0/16"
+
+  azs             = ["${var.aws_region}a"]
+  private_subnets = ["10.1.1.0/24"]
+  public_subnets  = ["10.1.101.0/24"]
+
+  enable_nat_gateway   = true
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  enable_dhcp_options  = true
+  single_nat_gateway   = true
+
+  tags = {
+    Environment = var.environment
+    Name        = var.project
+    Project     = var.project
+    Role        = "VPC"
+  }
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,13 @@ variable "aws_region" {
   description = "EC2 Region for the VPC"
   default     = "eu-central-1"
 }
+
+variable "project" {
+  description = "The resource project"
+  default     = "myproject"
+}
+
+variable "environment" {
+  description = "Environment for the resource"
+  default     = "team1"
+}
