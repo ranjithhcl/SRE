771363cba2ae73756de7bb7141c71d00620393d0
ranjithhcl <100259334+ranjithhcl@users.noreply.github.com>
2022-09-14 00:40:14 +0530

    Create acr.tf
    updated acr.tf

---
+++ azure_terraform/acr.tf
@@ -0,0 +1,3 @@
+module "container-registry_example_container_registry_with_private_endpoint" {
+  source  = "git::https://github.com/ranjithhcl/terraform-azurerm-container-registry.git//examples/container_registry_with_private_endpoint"
+}
